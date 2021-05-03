33bacade2abbacb5addfd41859556ee309181e43
harry <gaohaoyuandd@gmail.com>
2021-05-03 19:38:41 -0400

    reinstalled package.json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
+require('file-loader?name=[name].[ext]!./index.html');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
+import App from './App'; 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  ReactDOM.render(<div>hey</div>, root);
+  const root = document.getElementById("app");
+  ReactDOM.render(<App />, root);
 });
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    output:{
+        path: path.join(__dirname, 'dist'), 
+        filename:'index.bundle.js',
+    },
+    devServer:{
+        port: 8080,
+        watchContentBase: true,
+    },
+    module:{
+        rules: [
+            { 
+                test: /\.(js|jsx)?$/,
+                use:{
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
+            },
+            { 
+                test: /\.scss?$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+        ]
+    }
+};
