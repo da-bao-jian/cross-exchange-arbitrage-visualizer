6d8b358202a3fa34d72e187d4abd086cfac07444
harry <gaohaoyuandd@gmail.com>
2021-05-03 18:03:19 -0400

    finished webpack config setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,12 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./",
+    contentBase: path.resolve(__dirname ,"./public"),
     watchContentBase: true,
     open: "Chrome", // use "chrome" for PC
   },
