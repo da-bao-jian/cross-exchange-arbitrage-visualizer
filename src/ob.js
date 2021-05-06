d6f95c64788319bfa29f43a18179bd7bb9e3809a
harry <gaohaoyuandd@gmail.com>
2021-05-06 03:31:52 -0400

    created sebsocket_setup file and moved setup function into it

--- src/ob.js
+++ src/ob.js
@@ -3,6 +3,7 @@ import { Observable, timer } from 'rxjs';
 import { map, retryWhen, delayWhen, scan } from 'rxjs/operators'
 import {ws_bitmex$, ws_bitstamp$} from './utils/websocket_connection';
 import {subscribe_bitstamp, subscribe_bitmex} from './utils/websocket_message';
+import {bitstampSocketSetup} from './utils/websocket_setups';
 
 const OrderBook = () => {
 
@@ -16,30 +17,7 @@ const OrderBook = () => {
   const [bitstamp_orders, setBitstamp_orders] = useState();
   const [bitmex_orders, setbitmex_ordersOrders] = useState([]);
 
-  const bitstampSocketSetup = (currencyPair) => (
-    ws_bitstamp$
-    .multiplex(
-      () => ({event: 'bts:subscribe', data: {channel: `order_book_${currencyPair}`}}),
-      () => ({event: 'bts:unsubscribe', data: {channel: `order_book_${currencyPair}`}}),
-      (msg) => msg.channel === `order_book_${currencyPair}`
-    ) 
-    .pipe(
-      scan((accumulatedData, nextItem)=>{
-        if(Object.keys(nextItem['data']).length>0){//initialize a hash and setting keys asks and bids with orders on top of both sides
-          accumulatedData['bids'] = nextItem['data']['bids'][0];
-          accumulatedData['asks'] = nextItem['data']['asks'][0];
-        };
-        return accumulatedData;
-      },{}),
-      retryWhen((err) => { //error handling: reconnect if online, otherwise wait for internet connection
-        if (window.navigator.onLine) {
-          return timer(10000);
-        } else {
-          return fromEvent(window, 'online');
-        };
-      })
-    )
-  );
+
   //     bitstampSocket.subscribe(
   //         msg => {
   //           Object.keys(msg).length > 0 ? setBitstamp_orders(() => [msg]) : null;
---
+++ src/utils/websocket_setups.js
@@ -0,0 +1,30 @@
+import {ws_bitmex$, ws_bitstamp$} from './websocket_connection';
+import { retryWhen, scan } from 'rxjs/operators';
+import { timer } from 'rxjs';
+
+
+
+export const bitstampSocketSetup = (currencyPair) => (
+    ws_bitstamp$
+    .multiplex(
+        () => ({event: 'bts:subscribe', data: {channel: `order_book_${currencyPair}`}}),
+        () => ({event: 'bts:unsubscribe', data: {channel: `order_book_${currencyPair}`}}),
+        (msg) => msg.channel === `order_book_${currencyPair}`
+    ) 
+    .pipe(
+        scan((accumulatedData, nextItem)=>{
+        if(Object.keys(nextItem['data']).length>0){//initialize a hash and setting keys asks and bids with orders on top of both sides
+            accumulatedData['bids'] = nextItem['data']['bids'][0];
+            accumulatedData['asks'] = nextItem['data']['asks'][0];
+        };
+        return accumulatedData;
+        },{}),
+        retryWhen((err) => { //error handling: reconnect if online, otherwise wait for internet connection
+        if (window.navigator.onLine) {
+            return timer(10000);
+        } else {
+            return fromEvent(window, 'online');
+        };
+        })
+    )
+);
