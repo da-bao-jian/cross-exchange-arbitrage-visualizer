6da7fe6e30d762163e149cf3e226cd1bfff62f5e
harry <gaohaoyuandd@gmail.com>
2021-05-17 23:33:19 -0400

    added best bid and ask color and new gif

--- src/plot.jsx
+++ src/plot.jsx
@@ -5,100 +5,89 @@ const queue = (store, ele, capacity) => {
   if(store.length >= capacity){
     store.shift()
   };
-  if(!store.map((order)=>(order['askPrice'])).includes(ele['askPrice']) ||    
-    !store.map((order)=>(order['bidPrice'])).includes(ele['bidPrice'])||
-    !store.map((order)=>(order['bidSize'])).includes(ele['bidSize'])||
-    !store.map((order)=>(order['askSize'])).includes(ele['askSize'])
+  if(!store.map((order)=>(order['Price'])).includes(ele['Price']) ||    
+    !store.map((order)=>(order['Size'])).includes(ele['Size'])
   ){
     store.push(ele)
   };
   return store;
 };
 
-const randomeIdGenerator = (token) => (
+const idGenerator = (token) => (
     token+Math.random().toString()
 );
 
-const dataConverter = (exchange, order) => {
+const dataConverter = (exchange, order, side) => {
     return {
-        "id": randomeIdGenerator(exchange),
+        "id": idGenerator(exchange),
         "group": exchange,
-        "bidPrice": parseFloat(order['bids'][0]),
-        "bidSize": parseFloat(order['bids'][1]),
-        "askPrice": parseFloat(order['asks'][0]), 
-        "askSize": parseFloat(order['asks'][1])
-    }
+        "side": side,
+        "Price": parseFloat(order[0]),
+        "Size": parseFloat(order[1]),
+    };
 };
 
 export const Plot = (props) => {
-    let data;
-    let HASH = useRef({'bitstamp':[], 'bitmex':[], 'binance':[],'ftx':[], 'kraken':[], 'coinbase':[], 'bitfinex':[], 'bybit':[]});
-    let exchanges = Object.entries(props).filter((e)=> {
-        return e[1]!==undefined
-    });
-
+    let data, bidData, askData;
+    let bidHash = useRef({'bitstamp':[], 'bitmex':[], 'binance':[],'ftx':[], 'kraken':[], 'coinbase':[], 'bitfinex':[], 'bybit':[]});
+    let askHash = useRef({'bitstamp':[], 'bitmex':[], 'binance':[],'ftx':[], 'kraken':[], 'coinbase':[], 'bitfinex':[], 'bybit':[]});
+    let exchanges = Object.entries(props).filter((e)=> (e[1]!==undefined));
     let displayList = Object.keys(props).filter((e)=>(props[e]!==undefined));
 
-    if(exchanges.length > 0)
-    {
-        exchanges.map((exchange)=>{
-            HASH.current[exchange[0]] = queue(HASH.current[exchange[0]], dataConverter(exchange[0], exchange[1][0]), 10);
-        });
-        data = Object.values(HASH.current).filter(e=>{
-            return e[0]!==undefined && displayList.includes(e[0]['group'])
-        }).flat();
+    if(exchanges.length > 0){
+        exchanges.map((exchange)=>(bidHash.current[exchange[0]] = queue(bidHash.current[exchange[0]], dataConverter(exchange[0], exchange[1][0]['bids'], 'bid'), 3)));
+        exchanges.map((exchange)=>(askHash.current[exchange[0]] = queue(askHash.current[exchange[0]], dataConverter(exchange[0], exchange[1][0]['asks'], 'ask'), 3)));
+        bidData = Object.values(bidHash.current).filter(e=>(e[0]!==undefined && displayList.includes(e[0]['group']))).flat();
+        askData = Object.values(askHash.current).filter(e=>(e[0]!==undefined && displayList.includes(e[0]['group']))).flat();
+        data = bidData.concat(askData);
+        debugger
     };
-   
+
     return(
         <div style={{height: '600px', width: '100%'}}>
             {data !== undefined ?
-                <div style={{height:'100%'}}>
-                    <ResponsiveSwarmPlotCanvas    
-                        data={data}
-                        groups={displayList}
-                        value="bidPrice"
-                        valueFormat="$.2f"
-                        valueScale={{ type: 'linear', min: Math.min(...data.map(m=>m['bidPrice']))-10 , max: Math.max(...data.map(m=>m['bidPrice']))+10, reverse: false }}
-                        size={{ key: 'bidSize', values: [ 4, 20 ], sizes: [ 24, 80 ] }}
-                        gap={33}
-                        spacing={8}
-                        forceStrength={4}
-                        simulationIterations={260}
-                        borderColor={{
-                            from: 'color',
-                            modifiers: [
-                                [
-                                    'darker',
-                                    0.6
-                                ],
-                                [
-                                    'opacity',
-                                    0.5
+                <div style={{height:'60%'}}>
+                    <div style={{height:'100%', width:'100%'}}>
+                        <ResponsiveSwarmPlotCanvas
+                            data={data}
+                            groups={displayList}
+                            label='Size'
+                            value="Price"
+                            valueFormat="$.2f"
+                            valueScale={{ type: 'linear', min: Math.min(...bidData.map(m=>m['Price']))-5 , max: Math.max(...askData.map(m=>m['Price']))+5, reverse: false }}
+                            size={{ key: 'Size', values: [ 4, 20 ], sizes: [ 24, 80 ] }}
+                            gap={33}
+                            colorBy={({data})=>(data['side'])}
+                            spacing={8}
+                            forceStrength={4}
+                            simulationIterations={260}
+                            borderColor={{
+                                from: 'color',
+                                modifiers: [
+                                    [
+                                        'darker',
+                                        0.6
+                                    ],
+                                    [
+                                        'opacity',
+                                        0.5
+                                    ]
                                 ]
-                            ]
-                        }}
-                        margin={{ top: 80, right: 100, bottom: 80, left: 100 }}
-                        axisBottom={{
-                            orient: 'bottom',
-                            tickSize: 10,
-                            tickPadding: 5,
-                            tickRotation: 0,
-                            legend: 'Exchanges',
-                            legendPosition: 'middle',
-                            legendOffset: 46
-                        }}
-                        axisLeft={{
-                            orient: 'left',
-                            tickSize: 10,
-                            tickPadding: 5,
-                            tickRotation: 0,
-                            legend: 'Price(USD)',
-                            legendPosition: 'middle',
-                            legendOffset: -76
-                        }}
-                        motionStiffness={50}
-                        motionDamping={10}
-                    />
+                            }}
+                            margin={{ top: 80, right: 100, bottom: 0, left: 100 }}
+                            // axisTop={{
+                            //     orient: 'top',
+                            //     tickSize: 10,
+                            //     tickPadding: 5,
+                            //     tickRotation: 0,
+                            //     legend: 'Ask Price(USD)',
+                            //     legendPosition: 'middle',
+                            //     legendOffset: 46
+                            // }}
+                            motionStiffness={50}
+                            motionDamping={10}
+                        />
+                    </div>
                 </div>
             : null}
         </div>
